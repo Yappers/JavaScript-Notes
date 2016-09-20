2bf5809439f59621acacbe7b4e8ce69a1a9f5610
richardyap <richard.yapsh@gmail.com>
2016-09-20 22:18:53 +0100

    Input two numbers and output the result

--- js/test01.js
+++ js/test01.js
@@ -2,6 +2,16 @@ var num = addNum(5,10);
 
 document.getElementById("tigger").innerHTML = num;
 
+//A simple function to take in two variables and output a sum
 function addNum(num1,num2){
     return num1+num2;
+}
+
+
+function submit(){
+//    var input = document.getElementsByClassName("input-field");
+    var input1 = document.getElementById("input-field1").value;
+    var input2 = document.getElementById("input-field2").value;
+
+    document.getElementById("show").innerHTML = addNum(Number(input1),Number(input2));
 }
